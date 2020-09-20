2730cd9f44a90ac562679bca285192da9ee51552
Your Name <you@example.com>
2020-09-20 18:01:04 +0800

    Revert "Did a bunch of destructive stuff. There's more stuff here."
    This reverts commit 2f0b15ca5c916b44d94c0d530f8f00d4b626352e.

--- init.el
+++ init.el
@@ -28,10 +28,14 @@
        :ui
        deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       dasdjfhkalshfslhajsdfkjaksdfh
-       sadfasd
+       doom-dashboard    ; a nifty splash screen for Emacs
+       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       fill-column       ; a `fill-column' indicator
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       hydra
+       indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
-       mginimap           ; show a map of the code on the side
+       minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
